76fde2774eb4021263dfaaabfb00ac6e88f16393
Mitchell Peabody <mizhi@cognius.com>
2015-11-13 14:22:35 -0500

    Fix bug where an empty string would cause the stemAcceptable method to fail

--- src/test/scala/com/mizhi/nlp/stemmers/huskpaice/RuleSpec.scala
+++ src/test/scala/com/mizhi/nlp/stemmers/huskpaice/RuleSpec.scala
@@ -105,6 +105,10 @@ class RuleSpec extends UnitSpec {
       it("returns false when length is < 2") {
         rule.stemAcceptable("a") should be(false)
       }
+
+      it("works with empty string") {
+        rule.stemAcceptable("") should be(false)
+      }
     }
 
     describe("when word starts with consonant") {
@@ -125,5 +129,4 @@ class RuleSpec extends UnitSpec {
       }
     }
   }
-
 }
